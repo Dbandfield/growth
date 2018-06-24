4eb592751374129d243226e27f8de2210c04ae62
Dbandfield <daniel.bandfield"ntlworld.com>
2018-06-24 22:08:46 +0100

    added mongodb container. cant get auth to work.

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
-# PREREQUISITES
+# GROWTH
+
+## PREREQUISITES
 
 install docker https://www.docker.com/
 install npm https://www.npmjs.com/
 
-# ACCOUNT INFO
-You may need my docker credentials 
+## ACCOUNT INFO
 
-# BUILDING 
+You may need my docker credentials
+
+## BUILDING
 
 `run.sh` contains shortcuts to the docker commands\
 To build simply run `./run.sh build`\
 To build from scratch (slower but useful if the cache is causing problems) run `./run.sh rebuild`
 
-# RUNNING LOCALLY
+## RUNNING LOCALLY
 
 To start the server locally run `./run.sh start`\
 To build and then start the server run `./run.sh all`
-
-
-
-
--- app.js
+++ app.js
@@ -3,8 +3,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Mongo = require('mongodb');
 
 var indexRouter = require('./routes/index');
+var Generate = require('./server-lib/gr_generate.js');
+
+// host name is the name of the container in docker-compose.yml
+var MONGO_HOST = 'mongo';
+var dbName = 'planetarydb';
+// see docker-compose for environment vars
+var MONGO_URL = 'mongodb://' +  
+                process.env.ME_CONFIG_MONGODB_ADMINUSERNAME + ':' + 
+                process.env.ME_CONFIG_MONGODB_ADMINPASSWORD + '@' + 
+                MONGO_HOST + ':27017/' + dbName;
+console.log("Connecting to mongodb at " + MONGO_URL);
+var mongoClient = Mongo.MongoClient;
+mongoClient.connect(MONGO_URL, function(err, db) 
+{
+  if (err) throw err;
+  console.log(dbName + ' created!');
+
+  var dbo = db.db(dbName);
+  dbo.createCollection("planets", function(err, res) 
+  {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  }); // end createCollection
+}); // end connect
 
 var app = express();
 
@@ -35,5 +61,12 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// generate the universe
+var theUniverse = Generate.generateUniverse();
+for(var planet in theUniverse)
+{
+  //
+}
 
 module.exports = app;
